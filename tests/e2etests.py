a399b47fad4a6e547a754410df954df91b796a66
mrachuta <34251394+mrachuta@users.noreply.github.com>
2025-02-20 13:20:50 +0100

    Update path again

--- tests/e2etests.py
+++ tests/e2etests.py
@@ -8,7 +8,7 @@ class TestE2E(unittest.TestCase):
 
     def setUp(self):
         self.subprocess_required_args = {
-            "cwd": f"{os.path.join(os.path.dirname(os.path.abspath(__file__)), '../dist/')}",
+            "cwd": f"{os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'dist')}",
             "capture_output": True,
             "text": True,
         }
